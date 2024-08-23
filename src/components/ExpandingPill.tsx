6c7cfc657bfe1d07146bbd654169e54dd80befbb
GitDevla <pataki.david.ferenc@gmail.com>
2024-08-23 17:15:19 +0200

    Fix expading pill bug

--- src/components/ExpandingPill.tsx
+++ src/components/ExpandingPill.tsx
@@ -6,14 +6,12 @@ export default function ExpandingPill({
   children: React.ReactNode[];
 }) {
   return (
-    <div className="relative w-full gap-1">
-      <div className="group flex">
-        {defaultText}
-        <div className="flex w-0 max-w-0 gap-1 overflow-hidden transition-all group-hover:w-full group-hover:max-w-full">
-          {children.map((child, i) => (
-            <div key={i}>{child}</div>
-          ))}
-        </div>
+    <div className="relative flex w-full gap-1">
+      <div className="peer">{defaultText}</div>
+      <div className="flex w-0 gap-1 overflow-hidden transition-[width] hover:w-full peer-hover:w-full">
+        {children.map((child, i) => (
+          <div key={i}>{child}</div>
+        ))}
       </div>
     </div>
   );
