8371ba46bf1e01ccc34ed4536559d5b428ea8167
GitDevla <pataki.david.ferenc@gmail.com>
2024-08-26 15:50:39 +0200

    Add CDN to next config

--- next.config.mjs
+++ next.config.mjs
@@ -43,6 +43,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "noah.devla.dev",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
