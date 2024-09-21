b2a9117887b0b6117d255447996986bd1c703ec7
GitDevla <pataki.david.ferenc@gmail.com>
2024-09-21 21:44:17 +0200

    Refactor Hover component: Improve hover behavior and styling

--- src/components/Hover.tsx
+++ src/components/Hover.tsx
@@ -3,16 +3,16 @@ export default function Hover({
   className,
   hoverText,
 }: {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   className?: string;
   hoverText: string;
 }) {
   return (
-    <span className={`group relative underline ${className}`}>
-      {children}
-      <div className="absolute left-1/2 top-0 h-auto -translate-x-1/2 -translate-y-full whitespace-nowrap rounded-md border border-border bg-accentbackground p-2 opacity-0 transition-opacity group-hover:opacity-95">
+    <div className={`${className} relative inline`}>
+      <div className={`peer inline cursor-pointer`}>{children}</div>
+      <div className="pointer-events-none absolute left-1/2 top-0 h-auto -translate-x-1/2 -translate-y-full whitespace-nowrap rounded-md border border-border bg-accentbackground px-2 py-1 text-sm capitalize opacity-0 transition-opacity peer-hover:opacity-95">
         {hoverText}
       </div>
-    </span>
+    </div>
   );
 }
